ac48869ed19b55134c130ac0303d2f65f17205b8
moccch <cmo8@illinois.edu>
2025-10-23 18:37:20 -0500

    Fix

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,18 @@
 module.exports = function (router) {
 
-    var homeRoute = router.route('/');
+  var homeRoute = router.route('/');
 
-    homeRoute.get(function (req, res) {
-        var connectionString = process.env.TOKEN;
-        res.json({ message: 'My connection string is ' + connectionString });
+  homeRoute.get(function (req, res) {
+    res.json({ 
+      message: 'Welcome to MP3 API',
+      data: {
+        endpoints: {
+          users: '/api/users',
+          tasks: '/api/tasks'
+        }
+      }
     });
+  });
 
-    return router;
+  return router;
 }
